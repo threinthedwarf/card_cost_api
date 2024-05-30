9eb06410653321aa4293f568ff6d3f7d0959017f
vkoskotas <vasilis.koskotas@channelvas.com>
2024-05-30 08:45:40 +0300

    fix issue with validation of cardNumber

--- src/main/java/com/etraveli/card_cost_api/dto/PaymentsCardCostRequest.java
+++ src/main/java/com/etraveli/card_cost_api/dto/PaymentsCardCostRequest.java
@@ -2,11 +2,11 @@ package com.etraveli.card_cost_api.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.validator.constraints.Range;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -20,6 +20,6 @@ public class PaymentsCardCostRequest implements Serializable {
     private static final long serialVersionUID = 2900947566923758502L;
     @NotNull(message = "Card number is null.")
     @NotEmpty(message = "Card number is empty")
-    @Range(max = 19, min = 8, message = "Card number should be between 8 to 19 digits")
+    @Size(max = 19, min = 8, message = "Card number should be between 8 to 19 digits")
     private String cardNumber;
 }
